fa0a688b385cfd1cd7ce8f4bd405f45151cde189
Gaurav A. Sharma <35585319+DarkFusion90@users.noreply.github.com>
2018-10-14 22:33:25 +0530

    Added main .java file

---
+++ BattleshipGame.java
@@ -0,0 +1,434 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class BattleshipGame {
+	Player player1;
+	Player player2;
+	// #region description = "List of ships allowed in the game"
+
+	// Ship(length, maxQuantity)
+	// Total Ships on board: 1+2+2+3+4 = 10
+	public Ship carrier = new Ship(5, 1, "Carrier");
+	public Ship battleship = new Ship(4, 2, "Battleship");
+	public Ship cruiser = new Ship(3, 2, "Cruiser");
+	public Ship submarine = new Ship(3, 2, "Submarine");
+	public Ship destroyer = new Ship(2, 3, "Destroyer");
+	// #endregion
+	static public List<Ship> shipList = new ArrayList<>();
+	public Scanner keyboard = new Scanner(System.in);
+	final String NOTYETSHOT = "" , MISS = "0", HIT = "H", SUNK = "X", SHIP = "S";
+
+	BattleshipGame() {
+
+		do {
+			player1 = new Player();
+			player2 = new Player();
+			initializeShipList();
+
+			System.out.println("\n\nWelcome to the epic game: Battleships!");
+			System.out.println("Choose any one of the following: ");
+			boolean invalidInput = true;
+			String choice = null;
+
+			while (invalidInput) {
+				System.out.println("\n(1). Player vs Player\n(2). Player vs Computer");
+				choice = keyboard.nextLine();
+				invalidInput = !choice.equals("1") && !choice.equals("2");
+			}
+
+			if (choice.equals("1"))
+				PlayerVsPlayer();
+			else
+				PlayerVsComputer();
+
+			System.out.println("Press Y to play again. Else press any other key...");
+			choice = keyboard.nextLine();
+
+			if (!choice.equalsIgnoreCase("Y"))
+				break;
+		} while (true);
+	}
+
+	void initializeShipList() {
+		shipList.add(carrier);
+		shipList.add(battleship);
+
+		shipList.add(cruiser);
+		shipList.add(submarine);
+		shipList.add(destroyer);
+	}
+
+	void PlayerVsComputer() {
+		System.out.println("\nSsshhh! Old Grumpy Computer is sleeping! Come back later ;)\n");
+	}
+
+	void PlayerVsPlayer() {
+		System.out.println("\n\n\n\nPlayer 1's turn to input: ");
+		playerInput(player1);
+		System.out.println("Input for Player 1 completed...\nDisplaying grid: ");
+		player1.showGrid();
+
+		System.out.println("\n\nDisplaying Ships: ");
+
+		for (Map.Entry<String, List<List<Coordinate>>> ships : player1.fleetList.entrySet()) {
+			System.out.println(ships.getKey() + ": ");
+			for (List<Coordinate> shipCoordinates : ships.getValue()) {
+				System.out.print("[   ");
+				for (Coordinate coord : shipCoordinates) {
+					System.out.print("[" + coord.getX() + "," + coord.getY() + "]  ");
+				}
+				System.out.println("   ]");
+			}
+		}
+
+		do {
+			System.out.println("Player 2's turn to shoot: ");
+			while (true) {
+				Coordinate shotCoordinate = inputCoordinates(9, 9);
+				String response = shoot(player2, shotCoordinate);
+				if (response.equals("valid"))
+					break;
+			}
+
+		} while (true);
+
+		/*
+		 * System.out.println("\n\n\n\nPlayer 2's turn to input: "); playerInput();
+		 */
+	}
+
+	/**
+	 * 
+	 * @param currentPlayer The Player instance of the current player
+	 */
+	void playerInput(Player currentPlayer) {
+		for (Ship ship : BattleshipGame.shipList) {
+			// Example: "2 Cruiser ships of length 4"
+			int length = ship.getShipLength(), quantity = ship.getMaxShipQuantity();
+			String name = ship.getShipName();
+
+			System.out.println("\nYou need to enter: ");
+			System.out.println("" + quantity + " " + name + " ship(s) of length: " + length + "\n");
+			List<List<Coordinate>> currentShipList = new ArrayList<>();
+			while (quantity-- > 0) {
+				List<Coordinate> currentShip = addShip(length, currentPlayer);
+				currentShipList.add(currentShip);
+				updateGrid(currentPlayer, currentShip);
+
+				System.out.println("Succesfully created ship: " + name + "\nQuantity Remaining: " + quantity);
+			}
+			currentPlayer.addShipToFleet(name, currentShipList);
+		}
+	}
+
+	/**
+	 * @param length        Length of the ship to be added
+	 * @param currentPlayer The Player object of the current player
+	 */
+	List<Coordinate> addShip(int length, Player currentPlayer) {
+		final int HORIZONTAL = 1, VERTICAL = 2;
+		List<Coordinate> shipCoordinates = new ArrayList<>();
+
+		Coordinate rootCoordinate = new Coordinate(0, 0);
+		int horizontalOrVertical = -1;
+
+		do {
+			System.out.println("Choose orientation: \n(1). Horizontal\n(2). Vertical");
+			String temporary = keyboard.nextLine();
+
+			try {
+				horizontalOrVertical = Integer.parseInt(temporary);
+			} catch (NumberFormatException nfe) {
+				continue;
+			}
+
+			if (horizontalOrVertical > 0 && horizontalOrVertical < 3)
+				break;
+
+		} while (true);
+
+		if (horizontalOrVertical == HORIZONTAL)
+			rootCoordinate = inputCoordinates(10 - length, 9);
+		else if (horizontalOrVertical == VERTICAL)
+			rootCoordinate = inputCoordinates(9, 10 - length);
+
+		System.out.println("\nEntered cordinates: [" + rootCoordinate.getX() + "," + rootCoordinate.getY() + "]");
+
+		while (true) {
+			List<Coordinate> tempShip = new ArrayList<>();
+			tempShip.clear();
+
+			for (int i = 0; i < length; i++) {
+				switch (horizontalOrVertical) {
+				case HORIZONTAL:
+					tempShip.add(new Coordinate(rootCoordinate.getX() + i, rootCoordinate.getY()));
+					break;
+				case VERTICAL:
+					tempShip.add(new Coordinate(rootCoordinate.getX(), rootCoordinate.getY() + i));
+					break;
+				}
+			}
+
+			boolean validShip = true;
+			// if the list of ships of the current player is empty
+			// skip checking possible overlapping of ships in the grid
+			if (currentPlayer.fleetList.isEmpty()) {
+				shipCoordinates = tempShip;
+				break;
+			}
+
+			/*
+			 * if the entered ship coordinates overlap with any of the coordinates of the
+			 * currentPlayer's ships in the fleet, it is not a valid Ship and thus, we retry
+			 * adding shipCoordinates. Also, inform the user accordingly
+			 */
+
+			for (Map.Entry<String, List<List<Coordinate>>> ships : currentPlayer.fleetList.entrySet()) {
+				for (List<Coordinate> curentShipCoords : ships.getValue()) {
+					for (Coordinate coord : curentShipCoords) {
+						System.out.println("Searching Coordinates: [" + coord.getX() + "," + coord.getY() + "]");
+						if (containsCoordinate(tempShip, coord)) {
+							validShip = false;
+							// gets the first matching coordinate using Stream API
+							Coordinate firstMatchingCoord = tempShip.stream().filter(e -> e.equalsCoord(coord)).findFirst().get();
+							// print the first matching coordinate
+							System.out.println("There's a ship already present in [" + firstMatchingCoord.getX() + ","
+									+ firstMatchingCoord.getY() + "]");
+							System.out.println("Please enter the coordinates again\n");
+							// addShip again until valid Ship coord is entered
+							return addShip(length, currentPlayer);
+						}
+					}
+				}
+			}
+
+			if (validShip) {
+				shipCoordinates = tempShip;
+				break;
+			}
+		}
+		return shipCoordinates;
+	}
+
+	String shoot(Player currentplayer, Coordinate shotCoordinate) {
+		Player opponentPlayer = getOpponentPlayer(currentplayer);
+		String coordinateValue = getValueInCoord(opponentPlayer ,shotCoordinate);
+		if (coordinateValue.equals(NOTYETSHOT)) {
+			System.out.println("That's a Miss!");
+			updateGrid(opponentPlayer, shotCoordinate, MISS);
+			return "invalid";
+		}
+		else if (coordinateValue.equals(MISS)) {
+			System.out.println("You've already shot there! It was a miss.");
+			return "invalid";
+		}
+		else if (coordinateValue.equals(HIT)) {
+			System.out.println("You've already shot there! It was a hit.");
+			return "invalid";
+		}
+		else if (coordinateValue.equals(SUNK)) {
+			System.out.println("You've already shot there! That's one of the ships you sunk.");
+			return "invalid";
+		}
+		// coordinateValue.equals(SHIP);
+		else {
+			System.out.println("Thats a Hit!");
+			updateGrid(opponentPlayer, shotCoordinate, HIT);
+			return "valid";
+		}
+	}
+
+	/**
+	 * 
+	 * @param maximumX The maximum X-Coordinate allowed to entered
+	 * @param maximumY The maximum Y-Coordinate allowed to entered
+	 * @return Coordinate object with the entered co-ordinate value(X and Y)
+	 */
+	Coordinate inputCoordinates(int maximumX, int maximumY) {
+		int X, Y;
+
+		while (true) {
+			System.out.print("Enter X Co-ordinate (0-" + maximumX + "): ");
+			String temporary = keyboard.nextLine();
+			try {
+				X = Integer.parseInt(temporary);
+			} catch (NumberFormatException nfe) {
+				continue;
+			}
+			// if input is correct, stop trying and exit the loop
+			if (X >= 0 && X <= maximumX)
+				break;
+		}
+
+		while (true) {
+			System.out.print("Enter Y Co-ordinate (0-" + maximumY + "): ");
+			String temporary = keyboard.nextLine();
+
+			try {
+				Y = Integer.parseInt(temporary);
+			} catch (NumberFormatException nfe) {
+				continue;
+			}
+			// if input is correct, stop trying and exit the loop
+			if (Y >= 0 && Y <= maximumY)
+				break;
+		}
+
+		return new Coordinate(X, Y);
+	}
+
+	String getValueInCoord(Player player, Coordinate coordinate) {
+		int x = coordinate.getX();
+		int y = coordinate.getY();
+		return player.grid[x][y];
+	}
+
+	/**
+	 * 
+	 * @param invokingList The list to check for coordinates to match the next
+	 *                     parameter
+	 * @param coordinate   The coordinate to be matched with the listof coordinates
+	 *                     (invokingList)
+	 * @return True if atleast one element e is present in invokingList such that
+	 *         coordinate == e. Else, returns false
+	 */
+	boolean containsCoordinate(List<Coordinate> invokingList, Coordinate coordinate) {
+		for (Coordinate coord : invokingList) {
+			if (Coordinate.checkIfEqual(coord, coordinate))
+				return true;
+		}
+		return false;
+	}
+
+	void updateGrid(Player currentPlayer, List<Coordinate> shipCoordinates) {
+		for (Coordinate coordinate : shipCoordinates) {
+			int x = coordinate.getX();
+			int y = coordinate.getY();
+			currentPlayer.grid[x][y] = SHIP;
+		}
+	}
+	
+
+	void updateGrid(Player player, Coordinate coordinate , String value) {
+		int x = coordinate.getX();
+		int y = coordinate.getY();
+		player.grid[x][y] = value;
+	}
+	
+	Player getOpponentPlayer(Player currPlayer) {
+		return currPlayer == player1 ? player2 : player1;
+	}
+
+	public static void main(String args[]) {
+		new BattleshipGame();
+	}
+}
+
+class Ship {
+	int maxQuantity, shipLength;
+	String shipName;
+
+	/**
+	 * @param length   The length of the invoking ship
+	 * @param quantity The maximum number of the invoking ship's quantity allowed on
+	 *                 the board
+	 * @param name     The name of the ship
+	 */
+	Ship(int length, int quantity, String name) {
+		this.maxQuantity = quantity;
+		this.shipLength = length;
+		this.shipName = name;
+	}
+
+	/**
+	 * @return The invoking ship's length
+	 */
+	int getShipLength() {
+		return this.shipLength;
+	}
+
+	/**
+	 * @return The maximum number of the invoking ship's quantity allowed on the
+	 *         board
+	 */
+	int getMaxShipQuantity() {
+		return this.maxQuantity;
+	}
+
+	/**
+	 * @return The name of the invoking ship
+	 */
+	String getShipName() {
+		return this.shipName;
+	}
+
+}
+
+class Player {
+	final int ROWS = 10, COLUMNS = 10;
+	String[][] grid;
+	final int totalShips;
+
+	List<Coordinate> previousHitList = new ArrayList<>();
+	List<Coordinate> previousMissList = new ArrayList<>();
+	// Example: Map<"Cruiser" , List of Cruiser Ships<List of individual Cruiser
+	// Ship's coordinates>>
+	Map<String, List<List<Coordinate>>> fleetList = new HashMap<>();
+
+	Player() {
+		this.grid = makeGrid();
+		this.totalShips = 10;
+		this.lifeRemaining = 5;
+	}
+
+	String[][] makeGrid() {
+		String toReturn[][] = new String[ROWS][COLUMNS];
+		for (int i = 0; i < ROWS; i++) {
+			for (int j = 0; j < COLUMNS; j++) {
+				toReturn[i][j] = "";
+			}
+		}
+		return toReturn;
+	}
+
+	void showGrid() {
+		for (int i = 0; i < ROWS; i++) {
+			for (int j = 0; j < COLUMNS; j++) {
+				System.out.print(this.grid[j][i] + "\t");
+			}
+			System.out.println();
+		}
+	}
+
+	void addShipToFleet(String shipName, List<List<Coordinate>> newShipCoord) {
+		this.fleetList.put(shipName, newShipCoord);
+	}
+}
+
+class Coordinate {
+	private int X, Y;
+
+	Coordinate(int x, int y) {
+		this.X = x;
+		this.Y = y;
+	}
+
+	int getX() {
+		return this.X;
+	}
+
+	int getY() {
+		return this.Y;
+	}
+
+	static boolean checkIfEqual(Coordinate coord1, Coordinate coord2) {
+		return coord1.equalsCoord(coord2);
+	}
+
+	public boolean equalsCoord(Coordinate coordinate) {
+		return (this.X == coordinate.X) && (this.Y == coordinate.Y);
+	}
+}
